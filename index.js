bf0e83ad9d65b3ee62cdf56e4a646022637da9b9
Garry Kuwanto <gkuwanto@gmail.com>
2019-08-26 19:59:12 +0700

    added pwa

--- index.js
+++ index.js
@@ -104,3 +104,15 @@ $('#hitung').click(function(){
         alert(err.message)
     }
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
---
+++ sw.js
@@ -0,0 +1,48 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/main.min.css',
+  '/index.js',
+  '/bootstrap.min.css',
+  '/bootstrap.min.js',
+  '/jquery-3.3.1.slim.min.js',
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName){
+          return cacheName != CACHE_NAME
+        }).map(function(cacheName){
+          return caches.delete(cacheName)
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
